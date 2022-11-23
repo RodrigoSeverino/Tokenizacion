cee37468bb18f1276307107bc8508f0685fe6048
Rodrigo Ezequiel Severino <rseverino@prismamp.com>
2022-11-23 20:43:46 -0300

    add step and writer

--- src/main/java/com/Tokenizacion/Mappers/CardFileMapper.java
+++ src/main/java/com/Tokenizacion/Mappers/CardFileMapper.java
@@ -3,7 +3,6 @@ package com.Tokenizacion.Mappers;
 import com.Tokenizacion.DTO.CardDTO;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/Tokenizacion/Steps/GenerateTokenId.java
+++ src/main/java/com/Tokenizacion/Steps/GenerateTokenId.java
@@ -1,6 +1,9 @@
 package com.Tokenizacion.Steps;
 
+import ch.qos.logback.core.subst.Token;
+import com.Tokenizacion.DTO.TokenDTO;
 import com.Tokenizacion.Listener.JobCompleteListener;
+import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -8,6 +11,9 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,30 +22,52 @@ import org.springframework.context.annotation.Configuration;
 import com.Tokenizacion.DTO.CardDTO;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
+
 @Configuration
+@AllArgsConstructor
 @EnableBatchProcessing
 public class GenerateTokenId {
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
+
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
-
 
     @Bean
     public TokenProcessor processor() {
         return new TokenProcessor();
     }
 
+    private final ItemStreamReader<CardDTO> cardDTOItemStreamReader;
 
     @Bean
-    public ItemStreamReader<CardDTO> reader(LineMapper<CardDTO> lineMapper){
+    public FlatFileItemReader<CardDTO> reader(LineMapper<CardDTO> lineMapper){
         FlatFileItemReader<CardDTO> reader = new FlatFileItemReader<>();
         reader.setResource(new ClassPathResource("CDL_LoteTenisClub_2510_ConCDL_v1.txt"));
         reader.setLineMapper(lineMapper);
         return reader;
     }
 
+    @Bean
+    public JdbcBatchItemWriter<TokenDTO> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<TokenDTO>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO DA_Tokens (tokenId, status, fechaCreacion) VALUES (:tokenId, :status, :fechaCreacion)")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<TokenDTO> writer){
+        return stepBuilderFactory.get("step1")
+                .<CardDTO, TokenDTO>chunk(10)
+                .reader(reader((LineMapper<CardDTO>) cardDTOItemStreamReader))
+                .processor(processor())
+                .writer(writer)
+                .build();
+    }
     @Bean
     public Job tokenizacionMasiva(JobCompleteListener listener, Step step1) {
         return jobBuilderFactory.get("tokenizacionMasiva")
